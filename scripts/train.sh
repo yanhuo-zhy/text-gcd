9e406a7355c44d038efea0c1c54f7d9650e59600
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-23 08:50:57 +0000

    1

---
+++ scripts/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p amp20
+#SBATCH --qos amp20
+#SBATCH -N 1
+#SBATCH -c 5
+#SBATCH --mem=10000
+#SBATCH --gres gpu:1
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp1.txt
+
+module load gcc/gcc-10.2.0
+module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
+
+source /home/pszzz/miniconda3/bin/activate zhy
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+ --dataset_name='cifar10' \
+ --pseudo_num=10 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --experiment_name='cifar10_topk10_pseudo(10-15)'
