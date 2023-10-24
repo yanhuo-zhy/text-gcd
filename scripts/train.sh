bd212e63ac35eb4e2186204f4a8d1c9856cf8069
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-24 15:28:59 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -p amp20
-#SBATCH --qos amp20
+#SBATCH -p general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_scars.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_air0.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
