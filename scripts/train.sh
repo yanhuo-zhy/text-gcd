3e1b0146355f784811de6be43a2ad578589dfd34
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-25 09:21:04 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,13 +5,13 @@
 #SBATCH -c 5
 #SBATCH --mem=15000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cifar0.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
 
-# source /home/pszzz/miniconda3/bin/activate zhy
-source /home/psawl/miniconda3/bin/activate zhy
+source /home/pszzz/miniconda3/bin/activate zhy
+# source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cifar100' \
