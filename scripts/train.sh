4e663b4dd13865980dee51f86a8752c5f847f400
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-25 01:26:42 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,7 +5,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_air2.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cifar0.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -13,8 +13,8 @@ module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
 source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='aircraft' \
- --pseudo_ratio=0.6 \
+ --dataset_name='cifar100' \
+ --pseudo_ratio=0.4 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --experiment_name='aircraft_pseudoratio(0.6)_textaug'
+ --experiment_name='cifar100_pseudoratio(0.4)_textaug'
