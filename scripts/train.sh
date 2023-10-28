5b0e5aea010870b108c72672a3b203d68734c721
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-28 14:20:42 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account cs
-#SBATCH -p amp20
-#SBATCH --qos amp20
+#SBATCH --account cvl
+#SBATCH -p general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cifar0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_aircraft0.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,9 +15,9 @@ source /home/pszzz/miniconda3/bin/activate zhy
 # source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
+ --dataset_name='aircraft' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.3\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --experiment_name='cub_pseudoratio(0.6)_textaug_lambda(0.3)'
+ --experiment_name='aircraft_pseudoratio(0.6)_textaug_lambda(0.3)'
