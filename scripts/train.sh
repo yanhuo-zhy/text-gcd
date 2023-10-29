97754ac6564e55f6688c49a14004fc0a586f3251
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 03:41:48 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,7 +17,7 @@ source /home/pszzz/miniconda3/bin/activate zhy
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.0.35\
+ --lambda_loss=0.35\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --experiment_name='cub_pseudoratio(0.6)_textaug_lambda(0.35)'
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.0.3\
+ --lambda_loss=0.3\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --experiment_name='cub_pseudoratio(0.6)_textaug_lambda(0.3)'
