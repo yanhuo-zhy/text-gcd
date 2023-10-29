be0324d23d2722f728a1f278b2879a0dff3045ec
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 07:33:49 +0000

    1

--- scripts/get_tag.sh
+++ scripts/get_tag.sh
@@ -7,7 +7,9 @@
 #SBATCH --gres gpu:1
 #SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp0.txt
 
-module load nvidia/cuda-12.1 
-source /home/zhun.zhong/miniconda3/bin/activate zhy
+module load gcc/gcc-10.2.0
+module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
+
+source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python get_tag.py --dataset_name='imagenet'
--- scripts/get_tag_a100.sh
+++ scripts/get_tag_a100.sh
@@ -5,3 +5,8 @@
 #SBATCH --mem=30000
 #SBATCH --gres=gpu:a100.80:1
 #SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp0.txt
+
+module load nvidia/cuda-12.1 
+source /home/zhun.zhong/miniconda3/bin/activate zhy
+
+CUDA_VISIBLE_DEVICES=0 python get_tag.py --dataset_name='imagenet'
