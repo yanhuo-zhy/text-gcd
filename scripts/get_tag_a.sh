a8e0d607e76115097660eb6e6e6af3b736847580
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 12:09:19 +0000

    1

--- scripts/get_tag_a.sh
+++ scripts/get_tag_a.sh
@@ -5,8 +5,8 @@
 #SBATCH -N 1
 #SBATCH -t 2:00:00
 #SBATCH --mem=20000
-#SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp0.txt
+#SBATCH --gres=gpu:a100.40:1
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp1.txt
 
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
