311f7db57e158260b40bee527e64269faf3f59ab
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 09:35:09 +0000

    1

--- get_tag.py
+++ get_tag.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from lens import Lens, LensProcessor
 import torch
@@ -88,36 +89,43 @@ def generate_tags_attributes_for_one_batch(dataloader, lens):
 
     return tags_attributes_list
 
-def generate_tags_attributes_for_all_batches(dataloader, lens):
-    tags_attributes_list = []
+def generate_tags_attributes_for_batches(dataloader, lens, output_file, start_idx=0):
+    for batch_idx, batch in enumerate(tqdm(dataloader, desc="Processing batches", initial=start_idx, total=len(dataloader))):
+        if batch_idx < start_idx:
+            continue  # 跳过已经处理过的batches
 
-    # 循环遍历每一个batch
-    for batch in tqdm(dataloader, desc="Processing batches"):
         image_ids, samples = batch
         
         with torch.no_grad():
             outputs = lens(samples)
-            
+            tags_attributes_list = []
+
             for i, output in enumerate(outputs["prompts"]):
                 prompts = output
-                    
+
                 # 解析tags和attributes
                 lines = prompts.split('\n')
                 tags_start = lines.index('Tags:') + 1
                 attributes_start = lines.index('Attributes:') + 1
                 captions_start = lines.index('Captions:')
-                
+
                 tags = ["<tag>" + line.replace('-', '').strip() for line in lines[tags_start:attributes_start-1]][:5]
                 attributes = ["<attribute>" + line.replace('-', '').strip() for line in lines[attributes_start:captions_start]][:5]
-        
+
                 # 整合结果
                 result_str = image_ids[i] + "|||" + "|||".join(tags) + "|||" + "|||".join(attributes)
                 tags_attributes_list.append(result_str)
 
-    return tags_attributes_list
+            # 写入文件
+            save_to_file(tags_attributes_list, output_file)
 
-def save_to_file(data, filepath):
-    with open(filepath, 'w') as f:
+            # 更新当前处理到的batch索引
+            with open(output_file + '.idx', 'w') as f_idx:
+                f_idx.write(str(batch_idx + 1))
+
+
+def save_to_file(data, filepath, mode='a'):
+    with open(filepath, mode) as f:
         for line in data:
             f.write(line + '\n')
 
@@ -125,7 +133,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='cluster', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--batch_size', default=1000, type=int)
+    parser.add_argument('--batch_size', default=2, type=int)
     parser.add_argument('--gamma', type=float, default=0.1)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
@@ -144,6 +152,12 @@ if __name__ == "__main__":
 
     dataloader = DataLoader(lendataset, batch_size=args.batch_size, shuffle=False)
 
-    tags_attributes = generate_tags_attributes_for_all_batches(dataloader, lens)
-    # tags_attributes = generate_tags_attributes_for_one_batch(dataloader, lens)
-    save_to_file(tags_attributes, f'tag/{args.dataset_name}_tags_attributes.txt')
+    output_file = f'tag/{args.dataset_name}_tags_attributes.txt'
+    start_idx = 0
+    if os.path.exists(output_file + '.idx'):
+        with open(output_file + '.idx', 'r') as f_idx:
+            start_idx = int(f_idx.read().strip())
+
+    dataloader = DataLoader(lendataset, batch_size=args.batch_size, shuffle=False)
+
+    generate_tags_attributes_for_batches(dataloader, lens, output_file, start_idx=start_idx)
