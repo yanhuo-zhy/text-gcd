3423d82b57e8ce032bbd9b03cf3679c8af092ba2
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 02:53:43 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_aircraft1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_scars1.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,7 +15,7 @@ source /home/pszzz/miniconda3/bin/activate zhy
 # source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='aircraft' \
+ --dataset_name='scars' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2\
  --coteaching_epoch_t=10 \
