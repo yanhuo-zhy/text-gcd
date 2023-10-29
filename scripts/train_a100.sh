2e51fcaae4ef0814489cc68f07b55164af3cea9a
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 09:58:12 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 2:00:00
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub0.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub1.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 CUDA_VISIBLE_DEVICES=0 python train.py \
