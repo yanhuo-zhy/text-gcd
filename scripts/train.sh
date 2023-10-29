de8ea781853a9ef342207c6f955b1c591b28528b
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 02:50:12 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,7 +17,15 @@ source /home/pszzz/miniconda3/bin/activate zhy
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='scars' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.3\
+ --lambda_loss=0.35\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --experiment_name='scars_pseudoratio(0.6)_textaug_lambda(0.3)'
+ --experiment_name='scars_pseudoratio(0.6)_textaug_lambda(0.35)'
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+ --dataset_name='scars' \
+ --pseudo_ratio=0.6 \
+ --lambda_loss=0.35\
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --experiment_name='scars_pseudoratio(0.6)_textaug_lambda(0.25)'
