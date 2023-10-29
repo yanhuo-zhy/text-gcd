189e75c618165ba629e84d98abb583972cf19842
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-29 03:39:13 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,9 +4,9 @@
 #SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
-#SBATCH --mem=20000
+#SBATCH --mem=15000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cifar1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub0.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,9 +15,17 @@ source /home/pszzz/miniconda3/bin/activate zhy
 # source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cifar100' \
+ --dataset_name='cub' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.2\
+ --lambda_loss=0.0.35\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --experiment_name='cifar100_pseudoratio(0.6)_textaug_lambda(0.2)'
+ --experiment_name='cub_pseudoratio(0.6)_textaug_lambda(0.35)'
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+ --dataset_name='cub' \
+ --pseudo_ratio=0.6 \
+ --lambda_loss=0.0.3\
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --experiment_name='cub_pseudoratio(0.6)_textaug_lambda(0.3)'
