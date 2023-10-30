74eddc4525114ce7eac2a030a2354a51962717f4
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-10-30 14:33:40 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,15 +6,15 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar2.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cifar100' \
+ --dataset_name='cifar10' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=2 \
- --experiment_name='cifar100_pseudoratio(0.6)_textaug_lambda(0.2)_seed2'
+ --seed_num=0 \
+ --experiment_name='cifar10_pseudoratio(0.6)_textaug_lambda(0.2)_seed0'
