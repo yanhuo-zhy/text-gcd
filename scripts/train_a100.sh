b7ade5fcaaa9b289a5e0c6fcb46e85be9d94f279
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-01 06:17:48 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -5,25 +5,16 @@
 #SBATCH -t 2-0
 #SBATCH -N 1
 #SBATCH --mem=20000
-#SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_scars0.txt
+#SBATCH --gres=gpu:a100.80:1
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_food0.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='scars' \
+ --dataset_name='food' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=0 \
- --experiment_name='scars_pseudoratio(0.6)_textaug_lambda(0.2)_seed0'
-
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='scars' \
- --pseudo_ratio=0.6 \
- --lambda_loss=0.2\
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=2 \
- --experiment_name='scars_pseudoratio(0.6)_textaug_lambda(0.2)_seed2'
+ --experiment_name='food_pseudoratio(0.6)_textaug_lambda(0.2)_seed0'
