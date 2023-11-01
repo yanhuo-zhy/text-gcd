32729880ea7e5745375813b1f71421e2d2b3ab2b
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-01 06:27:44 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,15 +6,15 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar1.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
+ --dataset_name='cifar100' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2\
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
- --experiment_name='cub_ablation_w-co-teaching'
+ --experiment_name='cifar100_ablation_w-co-teaching'
