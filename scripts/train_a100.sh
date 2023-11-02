a40d044054a6439711bc292674470a851c840cc5
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-02 02:48:06 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -13,19 +13,19 @@ source /home/zhun.zhong/miniconda3/bin/activate zhy
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.9 \
+ --lambda_loss=0.7 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --experiment_name='cub_ablation_lambda(0.9)'
+ --experiment_name='cub_ablation_lambda(0.7)'
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
- --lambda_loss=0.8 \
+ --lambda_loss=0.6 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --experiment_name='cub_ablation_lambda(0.8)'
+ --experiment_name='cub_ablation_lambda(0.6)'
