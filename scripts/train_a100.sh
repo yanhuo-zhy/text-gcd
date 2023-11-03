e62395f638e914ff83c030ed587e930bb6e2f77e
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-03 11:52:04 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,16 +6,8 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_imagenet3.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='imagenet_1k' \
- --pseudo_ratio=0.6 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=1 \
- --interrupted_path='' \
- --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)_sharpen'
+CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py
