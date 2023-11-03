0628bbe294b79118d07e667979e35381f8c699f9
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-03 12:01:35 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,8 +6,16 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cubvith.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py
+CUDA_VISIBLE_DEVICES=0 python train.py \
+ --dataset_name='cub' \
+ --pseudo_ratio=0.6 \
+ --lambda_loss=0.2 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --seed_num=1 \
+ --interrupted_path='' \
+ --experiment_name='cub_vith16_test'
