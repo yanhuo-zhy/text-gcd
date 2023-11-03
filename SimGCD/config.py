6877a0b323e49e131d399f96eb5525b243f1178b
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-03 11:49:50 +0000

    1

--- SimGCD/config.py
+++ SimGCD/config.py
@@ -3,7 +3,7 @@
 # -----------------
 cifar_10_root = '${DATASET_DIR}/cifar10'
 cifar_100_root = '${DATASET_DIR}/cifar100'
-cub_root = '/wang_hp/zhy/data'
+cub_root = '/home/zhun.zhong/GCD_dataset/cub'
 aircraft_root = '${DATASET_DIR}/fgvc-aircraft-2013b'
 car_root = '/wang_hp/zhy/gcd-task/data/cars'
 herbarium_dataroot = '${DATASET_DIR}/herbarium_19'
