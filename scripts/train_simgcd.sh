b6b5963ac768ddb0553b010c0b812731f09f603a
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-03 13:04:07 +0000

    1

--- scripts/train_simgcd.sh
+++ scripts/train_simgcd.sh
@@ -15,12 +15,4 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/pszzz/miniconda3/bin/activate zhy
 # source /home/psawl/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.6 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=1 \
- --interrupted_path='' \
- --experiment_name='cub_vith16_test'
+CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py 
