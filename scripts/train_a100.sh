c4fad8e733afe89fc8340cbdc234343d8c788bd6
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-04 11:14:51 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,50 +6,28 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar100_0.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.5 \
+ --dataset_name='cifar100' \
+ --pseudo_ratio=0.1 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cub_ablation_pseudo_ratio(0.5)'
+ --experiment_name='cifar100_ablation_pseudo_ratio(0.1)'
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.7 \
+ --dataset_name='cifar100' \
+ --pseudo_ratio=0.2 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cub_ablation_pseudo_ratio(0.7)'
-
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.8 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=1 \
- --interrupted_path='' \
- --batch_size=128 \
- --experiment_name='cub_ablation_pseudo_ratio(0.8)'
-
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.9 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=1 \
- --interrupted_path='' \
- --batch_size=128 \
- --experiment_name='cub_ablation_pseudo_ratio(0.9)'
+ --experiment_name='cifar100_ablation_pseudo_ratio(0.2)'
