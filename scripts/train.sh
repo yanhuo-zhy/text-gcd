766641ceb93e00bf4e9af69311618fd311e18b93
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-04 09:45:17 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -17,10 +17,10 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
- --pseudo_ratio=0.1 \
+ --pseudo_ratio=0.2 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --experiment_name='cub_ablation_pseudo_ratio(0.1)'
+ --experiment_name='cub_ablation_pseudo_ratio(0.2)'
