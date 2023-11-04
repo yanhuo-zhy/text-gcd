f3da5dc47c3988bee071018bb7643d6afdc2c3ff
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-04 11:01:57 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -18,5 +18,5 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --batch_size=120 \
- --experiment_name='cub_vit_h_ours_bs120'
+ --batch_size=110 \
+ --experiment_name='cub_vit_h_ours_bs110'
