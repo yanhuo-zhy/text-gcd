7565cde42e4b0d426769beeba00371ae440aeeec
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-05 09:57:05 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_6.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cub0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -16,12 +16,12 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cifar100' \
+ --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=5 \
- --coteaching_epoch_i=10 \
+ --coteaching_epoch_t=0 \
+ --coteaching_epoch_i=0 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cifar100_ablation_warmup5-10'
+ --experiment_name='cub_ablation_warmup0-0'
