6c81361693191cdf49e6a256a6af6453628c772e
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-05 07:05:29 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_vith0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_vith1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -19,8 +19,8 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=20 \
- --coteaching_epoch_i=20 \
+ --coteaching_epoch_t=25 \
+ --coteaching_epoch_i=25 \
  --seed_num=1 \
  --interrupted_path='' \
- --experiment_name='cub_vith_fixbackbone_warmup20-20'
+ --experiment_name='cub_vith_fixbackbone_warmup25-25'
