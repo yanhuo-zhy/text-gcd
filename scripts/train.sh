519aa72993e07d4f59980e6376824884ca5777e4
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-05 10:04:36 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #SBATCH --account cs
-#SBATCH -p general 
-#SBATCH --qos normal  
+#SBATCH -p ampere 
+#SBATCH --qos ampere  
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cub2.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 
-# source /home/pszzz/miniconda3/bin/activate zhy
-source /home/psawl/miniconda3/bin/activate zhy
+source /home/pszzz/miniconda3/bin/activate zhy
+# source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=5 \
- --coteaching_epoch_i=10 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cub_ablation_warmup5-10'
+ --experiment_name='cub_ablation_1tag_0attribute'
