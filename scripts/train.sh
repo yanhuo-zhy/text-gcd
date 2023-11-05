8356bc2ffc06bb6a5779812aa3276ff35bc38d29
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-05 09:19:03 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=30000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_1.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_2.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -17,22 +17,22 @@ source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cifar100' \
- --pseudo_ratio=0.3 \
+ --pseudo_ratio=0.5 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cifar100_ablation_pseudo_ratio(0.3)'
+ --experiment_name='cifar100_ablation_pseudo_ratio(0.5)'
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cifar100' \
- --pseudo_ratio=0.4 \
+ --pseudo_ratio=0.7 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cifar100_ablation_pseudo_ratio(0.4)'
+ --experiment_name='cifar100_ablation_pseudo_ratio(0.7)'
