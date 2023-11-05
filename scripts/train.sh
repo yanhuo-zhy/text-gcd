be08877dd8a28ff31543cb9d1abe1c07232d01b6
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-05 09:36:18 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account cs
-#SBATCH -p amp48
-#SBATCH --qos amp48
+#SBATCH -p cs
+#SBATCH --qos csstaff 
 #SBATCH -N 1
 #SBATCH -c 5
-#SBATCH --mem=30000
+#SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_3.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_4.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -17,22 +17,11 @@ source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='cifar100' \
- --pseudo_ratio=0.8 \
+ --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
+ --coteaching_epoch_t=0 \
+ --coteaching_epoch_i=0 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cifar100_ablation_pseudo_ratio(0.8)'
-
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cifar100' \
- --pseudo_ratio=0.9 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=1 \
- --interrupted_path='' \
- --batch_size=128 \
- --experiment_name='cifar100_ablation_pseudo_ratio(0.9)'
+ --experiment_name='cifar100_ablation_warmup0-0'
