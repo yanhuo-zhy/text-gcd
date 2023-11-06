ff0293d3f20b8cfd2794965f3491336625b94aed
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-06 07:43:09 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,9 +4,9 @@
 #SBATCH --qos amp48  
 #SBATCH -N 1
 #SBATCH -c 5
-#SBATCH --mem=20000
+#SBATCH --mem=30000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_vith1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_imagenet1k_0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -16,12 +16,12 @@ source /home/pszzz/miniconda3/bin/activate zhy
 # source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
+ --dataset_name='imagenet_1k' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=25 \
- --coteaching_epoch_i=20 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --batch_size=128 \
- --experiment_name='cub_vith_fixbackbone_warup_25_20'
+ --batch_size=512 \
+ --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)'
