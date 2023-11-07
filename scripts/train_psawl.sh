84363453b397b955ec30f8c4e448a4a06eb8a2e1
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-07 10:22:14 +0000

    1

--- scripts/train_psawl.sh
+++ scripts/train_psawl.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_tag0.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_tag1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -23,4 +23,4 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cifar100_ablation_text(1-0)'
+ --experiment_name='cifar100_ablation_text(2-0)'
