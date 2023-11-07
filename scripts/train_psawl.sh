0a4042e52781acbf34c77366e3cb13be7d9ab375
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-07 10:18:49 +0000

    1

--- scripts/train_psawl.sh
+++ scripts/train_psawl.sh
@@ -6,22 +6,21 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_imagenet1k_1.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_tag0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 
-source /home/pszzz/miniconda3/bin/activate zhy
-# source /home/psawl/miniconda3/bin/activate zhy
+source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='imagenet_1k' \
+ --dataset_name='cifar100' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
- --batch_size=512 \
- --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)_sharpen'
+ --batch_size=128 \
+ --experiment_name='cifar100_ablation_text(1-0)'
