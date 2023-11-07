31d57c2a14d841b806c6b0df0f65278c3cceb2df
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-07 11:12:21 +0000

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,17 +6,17 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub_vith3.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub_vith4.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
+CUDA_VISIBLE_DEVICES=0 python train_vith.py \
  --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=20 \
+ --coteaching_epoch_t=15 \
  --coteaching_epoch_i=20 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=110 \
- --experiment_name='cub_vith__warup20-20'
+ --experiment_name='cub_vith__warup15-20'
