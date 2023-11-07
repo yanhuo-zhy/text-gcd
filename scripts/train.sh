b918d98ee09c6469080e56722346cc663058d28e
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-07 10:43:54 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_6.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_7.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -23,4 +23,4 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cub_ablation_text(3-0)'
+ --experiment_name='cub_ablation_text(3-1)'
