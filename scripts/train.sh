fda627544c409c2e57584720e804a4a99da4f64f
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-07 10:55:54 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=30000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_imagenet_1k_1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_imagenet_1k_2.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -18,9 +18,9 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='imagenet_1k' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=1 \
- --coteaching_epoch_i=5 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)_warmup1-5_sharpen'
+ --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)'
