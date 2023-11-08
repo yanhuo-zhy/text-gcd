4dd1efb616682cb9a2c43cb4617a9e3c2a0e0f3e
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-08 03:04:04 +0000

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account cvl
-#SBATCH -p amp48 
-#SBATCH --qos amp48  
+#SBATCH -p amp20
+#SBATCH --qos amp20
 #SBATCH -N 1
 #SBATCH -c 5
-#SBATCH --mem=30000
+#SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_imagenet_1k_2.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_soft.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -14,8 +14,8 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 
 source /home/pszzz/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='imagenet_1k' \
+CUDA_VISIBLE_DEVICES=0 python train_soft.py \
+ --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
@@ -23,4 +23,4 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='imagenet_1k_pseudoratio(0.6)_textaug_lambda(0.2)'
+ --experiment_name='cub_ablation_soft'
