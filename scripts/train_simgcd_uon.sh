bdbef304c50d9835b4925b03068f1d3bfc1c2088
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-10 02:06:34 +0000

    1

---
+++ scripts/train_simgcd_uon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account cvl
+#SBATCH -p amp20
+#SBATCH --qos amp20
+#SBATCH -N 1
+#SBATCH -c 5
+#SBATCH --mem=20000
+#SBATCH --gres gpu:1
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_simgcd_cub0.txt
+
+module load gcc/gcc-10.2.0
+# module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
+module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
+
+source /home/pszzz/miniconda3/bin/activate zhy
+
+CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
+ --dataset_name='cub' \
+ --exp_name='SimGCD-clipvitb-cub'
