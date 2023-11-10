8c3463dd64e6b2f518e16ecb0664de5a0a31b923
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-10 07:58:20 +0000

    1

--- scripts/train_simgcd.sh
+++ scripts/train_simgcd.sh
@@ -6,10 +6,11 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_vith.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_vith_fix.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
- --dataset_name='cub' \
- --exp_name='SimGCD-clipvitb-cub'
+CUDA_VISIBLE_DEVICES=0 python SimGCD/train_vith.py \
+ --dataset_name='cifar100' \
+ --exp_name='SimGCD-clipvith-cifar100-fix' \
+ --print_freq=20
