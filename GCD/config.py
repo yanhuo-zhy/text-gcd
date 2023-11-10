30a514f26fdb609daa6dadbfbf23f8a8040adfe5
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-10 23:22:56 +0800

    Add files via upload

---
+++ GCD/config.py
@@ -0,0 +1,26 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '${DATASET_DIR}/cifar10'
+cifar_100_root = '${DATASET_DIR}/cifar100'
+cub_root = '/wang_hp/zhy/data'
+aircraft_root = '${DATASET_DIR}/fgvc-aircraft-2013b'
+car_root = '/wang_hp/zhy/data/stanford_cars'
+herbarium_dataroot = '${DATASET_DIR}/herbarium_19'
+imagenet_root = '${DATASET_DIR}/ImageNet'
+
+oxford_pet_root = '/wang_hp/zhy/data'
+
+oxford_flowers_root = '/wang_hp/zhy/data'
+
+food_101_root = '/wang_hp/zhy/data/food-101'
+
+dino_pretrain_path = '/root/.cache/torch/hub/checkpoints/dino_vitbase16_pretrain.pth'
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
