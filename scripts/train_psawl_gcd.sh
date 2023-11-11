f3cd3072f7718f221b02d64f33c7cc6d6dfc18e1
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-11 01:23:40 +0000

    1

--- scripts/train_psawl_gcd.sh
+++ scripts/train_psawl_gcd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account cvl
-#SBATCH -p general 
-#SBATCH --qos normal  
+#SBATCH -p amp20 
+#SBATCH --qos amp20  
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_flowers.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_food.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,5 +15,5 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python GCD/contrastive_learning.py \
- --dataset_name='flowers' \
- --exp_name='gcd_flowers_clip'
+ --dataset_name='food' \
+ --exp_name='gcd_food_clip'
