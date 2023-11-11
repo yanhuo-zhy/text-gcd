b2b2ac0bf23628b6f3599e14b552a48c50e04eab
yanhuo-zhy <1760331284@qq.com>
2023-11-11 20:39:02 +0800

    1

--- scripts/train_psawl_gcd.sh
+++ scripts/train_psawl_gcd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account cvl
-#SBATCH -p amp20 
-#SBATCH --qos amp20  
+#SBATCH -p amp48 
+#SBATCH --qos amp48  
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_food.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_cifar100_100epoch.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,5 +15,6 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python GCD/contrastive_learning.py \
- --dataset_name='food' \
- --exp_name='gcd_food_clip'
+ --dataset_name='cifar100' \
+ --exp_name='gcd_food_cifar100_100epoch' \
+ --epochs=100
