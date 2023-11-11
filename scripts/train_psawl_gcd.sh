20507f5fd11a0dfe5d5c2a849e008a6afa45bd58
yanhuo-zhy <1760331284@qq.com>
2023-11-11 20:47:43 +0800

    1

--- scripts/train_psawl_gcd.sh
+++ scripts/train_psawl_gcd.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_cub_100epoch.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_flowers_100epoch.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,6 +15,6 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python GCD/contrastive_learning.py \
- --dataset_name='cub' \
- --exp_name='gcd_food_cub_100epoch' \
+ --dataset_name='flowers' \
+ --exp_name='gcd_food_flowers_100epoch' \
  --epochs=100
