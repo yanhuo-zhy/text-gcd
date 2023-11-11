33eb82f4f5e0ef798352c7c44443c48a1925a8c1
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2023-11-11 01:17:07 +0000

    1

--- scripts/train_psawl_gcd.sh
+++ scripts/train_psawl_gcd.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_pets.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/GCD/temp_gcd_clip_flowers.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,5 +15,5 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python GCD/contrastive_learning.py \
- --dataset_name='pets' \
- --exp_name='gcd_pets_clip'
+ --dataset_name='flowers' \
+ --exp_name='gcd_flowers_clip'
