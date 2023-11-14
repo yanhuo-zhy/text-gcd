5e82fa01e4a7f8233661fe28b852507d2d031197
yanhuo-zhy <1760331284@qq.com>
2023-11-14 08:39:25 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account cvl
-#SBATCH -p amp20
-#SBATCH --qos amp20
+#SBATCH -p general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_coca0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_coca1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=0 \
+ --seed_num=2 \
  --interrupted_path='' \
  --batch_size=128 \
- --experiment_name='cub_coca_seed0'
+ --experiment_name='cub_coca_seed2'
