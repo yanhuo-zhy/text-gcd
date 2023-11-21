3c2df2c626daa1052915f3c23a42891a8a439370
yanhuo-zhy <1760331284@qq.com>
2023-11-21 21:31:50 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_probtrain1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_probtrain2.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -23,5 +23,5 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --seed_num=0 \
  --interrupted_path='' \
  --batch_size=128 \
- --prop_train_labels=0.2 \
- --experiment_name='cub_prop_train_labels_0.2_seed0'
+ --prop_train_labels=0.3 \
+ --experiment_name='cub_prop_train_labels_0.3_seed0'
