d9a3305196af10028f3da96452c4deb70ff79ed8
yanhuo-zhy <1760331284@qq.com>
2023-11-22 10:14:18 +0800

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,58 +6,32 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cub_konwnclass1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar100_konwnclass0.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
- --dataset_name='cub' \
+ --dataset_name='cifar100' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=2 \
+ --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
  --prop_knownclass=0.1 \
- --experiment_name='cub_knownclass_0.1_seed2'
+ --experiment_name='cifar100_knownclass_0.1_seed1'
 
 CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
- --dataset_name='cub' \
+ --dataset_name='cifar100' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=2 \
+ --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
  --prop_knownclass=0.2 \
- --experiment_name='cub_knownclass_0.2_seed2'
-
-CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.6 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=2 \
- --interrupted_path='' \
- --batch_size=128 \
- --prop_train_labels=0.5 \
- --prop_knownclass=0.3 \
- --experiment_name='cub_knownclass_0.3_seed2'
-
-CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
- --dataset_name='cub' \
- --pseudo_ratio=0.6 \
- --lambda_loss=0.2 \
- --coteaching_epoch_t=10 \
- --coteaching_epoch_i=15 \
- --seed_num=2 \
- --interrupted_path='' \
- --batch_size=128 \
- --prop_train_labels=0.5 \
- --prop_knownclass=0.4 \
- --experiment_name='cub_knownclass_0.4_seed2'
+ --experiment_name='cifar100_knownclass_0.2_seed1'
