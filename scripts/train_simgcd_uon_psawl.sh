6fd9bce7ddcf59565ee09162f2eade70fae98a83
yanhuo-zhy <1760331284@qq.com>
2023-11-23 00:38:29 +0800

    1

--- scripts/train_simgcd_uon_psawl.sh
+++ scripts/train_simgcd_uon_psawl.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_simgcd_cub_prop_knowclass0.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_simgcd_cub_prop_knowclass1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -18,14 +18,14 @@ CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
  --dataset_name='cub' \
  --seed_num=0 \
  --prop_train_labels=0.5 \
- --prop_knownclass=0.1 \
- --exp_name='True_SimGCD_cub_prob_knowclass(0.1)_seed0' \
+ --prop_knownclass=0.3 \
+ --exp_name='True_SimGCD_cub_prob_knowclass(0.3)_seed0' \
  --print_freq=20
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
  --dataset_name='cub' \
  --seed_num=0 \
  --prop_train_labels=0.5 \
- --prop_knownclass=0.2 \
- --exp_name='True_SimGCD_cub_prob_knowclass(0.2)_seed0' \
+ --prop_knownclass=0.4 \
+ --exp_name='True_SimGCD_cub_prob_knowclass(0.4)_seed0' \
  --print_freq=20
