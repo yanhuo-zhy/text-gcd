e0d26d16b55a7eb989dc8cd57e47204db4480047
yanhuo-zhy <1760331284@qq.com>
2023-11-22 11:09:15 +0800

    1

--- scripts/train_simgcd.sh
+++ scripts/train_simgcd.sh
@@ -6,22 +6,22 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_trainlabels1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_trainlabels2.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
  --dataset_name='cifar100' \
- --seed_num=0 \
- --prop_train_labels=0.3 \
+ --seed_num=1 \
+ --prop_train_labels=0.1 \
  --prop_knownclass=0.8 \
- --exp_name='SimGCD_cifar100_prob_trainlabels(0.3)_seed0' \
+ --exp_name='SimGCD_cifar100_prob_trainlabels(0.1)_seed1' \
  --print_freq=20
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
  --dataset_name='cifar100' \
- --seed_num=0 \
- --prop_train_labels=0.4 \
+ --seed_num=1 \
+ --prop_train_labels=0.2 \
  --prop_knownclass=0.8 \
- --exp_name='SimGCD_cifar100_prob_trainlabels(0.4)_seed0' \
+ --exp_name='SimGCD_cifar100_prob_trainlabels(0.2)_seed1' \
  --print_freq=20
