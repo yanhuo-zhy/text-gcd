5dc26773eb3ae11737067b43792726ea74469a0d
yanhuo-zhy <1760331284@qq.com>
2023-11-23 00:37:32 +0800

    1

--- scripts/train_simgcd_uon_psawl.sh
+++ scripts/train_simgcd_uon_psawl.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_trainlabels5.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_simgcd_cub_prop_knowclass0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,9 +15,17 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/psawl/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
- --dataset_name='cifar100' \
- --seed_num=2 \
- --prop_train_labels=0.4 \
- --prop_knownclass=0.8 \
- --exp_name='True_SimGCD_cifar100_prob_trainlabels(0.4)_seed2' \
+ --dataset_name='cub' \
+ --seed_num=0 \
+ --prop_train_labels=0.5 \
+ --prop_knownclass=0.1 \
+ --exp_name='True_SimGCD_cub_prob_knowclass(0.1)_seed0' \
+ --print_freq=20
+
+CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
+ --dataset_name='cub' \
+ --seed_num=0 \
+ --prop_train_labels=0.5 \
+ --prop_knownclass=0.2 \
+ --exp_name='True_SimGCD_cub_prob_knowclass(0.2)_seed0' \
  --print_freq=20
