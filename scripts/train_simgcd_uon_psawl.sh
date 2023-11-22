bf2d86e5252998e8bc67f77662c005015a031503
yanhuo-zhy <1760331284@qq.com>
2023-11-23 00:19:57 +0800

    1

--- scripts/train_simgcd_uon_psawl.sh
+++ scripts/train_simgcd_uon_psawl.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_simgcd_cub_prob0.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_trainlabels0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,7 +15,9 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
- --dataset_name='imagenet_1k' \
- --exp_name='SimGCD-clipvitb-imagenet_1k' \
- --print_freq=10 \
- --num_workers=6
+ --dataset_name='cifar100' \
+ --seed_num=1 \
+ --prop_train_labels=0.3 \
+ --prop_knownclass=0.8 \
+ --exp_name='True_SimGCD_cifar100_prob_trainlabels(0.3)_seed1' \
+ --print_freq=20
