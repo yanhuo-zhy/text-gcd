2728321c02c064b34b6b5b0ba9fa810a50e97e7b
yanhuo-zhy <1760331284@qq.com>
2023-11-22 10:23:50 +0800

    1

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -6,7 +6,7 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.80:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar100_konwnclass1.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_cifar100_konwnclass2.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
@@ -16,12 +16,12 @@ CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=1 \
+ --seed_num=0 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --prop_knownclass=0.3 \
- --experiment_name='cifar100_knownclass_0.3_seed1'
+ --prop_knownclass=0.1 \
+ --experiment_name='cifar100_knownclass_0.1_seed0'
 
 CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
  --dataset_name='cifar100' \
@@ -29,9 +29,9 @@ CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=1 \
+ --seed_num=0 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --prop_knownclass=0.4 \
- --experiment_name='cifar100_knownclass_0.4_seed1'
+ --prop_knownclass=0.2 \
+ --experiment_name='cifar100_knownclass_0.2_seed0'
