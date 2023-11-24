849d80fd8e563922ca3c83469d977d936af5bb58
yanhuo-zhy <1760331284@qq.com>
2023-11-24 14:52:13 +0800

    1

--- scripts/train_psawl.sh
+++ scripts/train_psawl.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_konwnclass0.txt
+#SBATCH -o /home/psawl/hyzheng/text-gcd/temp/temp_cifar100_konwnclass1.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -20,9 +20,9 @@ CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=0 \
+ --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
  --prop_knownclass=0.5 \
- --experiment_name='cifar100_knownclass_0.5_seed0'
+ --experiment_name='cifar100_knownclass_0.5_seed1'
