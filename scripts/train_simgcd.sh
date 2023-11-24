31f71e6309a856fe2b002555e65e9f55cdb68cc0
yanhuo-zhy <1760331284@qq.com>
2023-11-24 14:32:02 +0800

    1

--- scripts/train_simgcd.sh
+++ scripts/train_simgcd.sh
@@ -6,14 +6,14 @@
 #SBATCH -N 1
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:a100.40:1
-#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_knownclass2.txt
+#SBATCH -o /home/zhun.zhong/hyzheng/text-gcd/temp/temp_simgcd_cifar100_prop_knownclass3.txt
 module load cuda/12.1
 source /home/zhun.zhong/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
  --dataset_name='cifar100' \
- --seed_num=2 \
+ --seed_num=0 \
  --prop_train_labels=0.5 \
- --prop_knownclass=0.1 \
- --exp_name='SimGCD_cifar100_prob_knownclass(0.1)_seed2' \
+ --prop_knownclass=0.2 \
+ --exp_name='SimGCD_cifar100_prob_knownclass(0.2)_seed0' \
  --print_freq=20
