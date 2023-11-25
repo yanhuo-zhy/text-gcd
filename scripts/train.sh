31f6a9a85b5a57b27a81d60c85d0c0e67d35eae6
yanhuo-zhy <1760331284@qq.com>
2023-11-25 10:15:15 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/get_tag_blip2_cifar100.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_blip2_cub0.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -14,15 +14,15 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 
 source /home/pszzz/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python get_tag_blip.py
-# CUDA_VISIBLE_DEVICES=0 python train.py \
-#  --dataset_name='cifar100' \
-#  --pseudo_ratio=0.6 \
-#  --lambda_loss=0.2 \
-#  --coteaching_epoch_t=15 \
-#  --coteaching_epoch_i=10 \
-#  --seed_num=1 \
-#  --interrupted_path='' \
-#  --batch_size=128 \
-#  --prop_train_labels=0.5 \
-#  --experiment_name='blip_cifar100_warmup(15-10)'
+# CUDA_VISIBLE_DEVICES=0 python get_tag_blip.py
+CUDA_VISIBLE_DEVICES=0 python train.py \
+ --dataset_name='cub' \
+ --pseudo_ratio=0.6 \
+ --lambda_loss=0.2 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --seed_num=1 \
+ --interrupted_path='' \
+ --batch_size=128 \
+ --prop_train_labels=0.5 \
+ --experiment_name='blip2_cub_warmup(10-15)'
