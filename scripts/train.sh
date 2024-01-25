8ddce70ab7681bc0ca0cc3bc57e48fc5c5aac30e
yanhuo-zhy <1760331284@qq.com>
2024-01-25 11:50:29 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_rebuttal_scars1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_rebuttal_scars2.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -20,9 +20,9 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=5 \
- --coteaching_epoch_i=5 \
+ --coteaching_epoch_i=10 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='rebuttal_scars_5-5_seed1'
+ --experiment_name='rebuttal_scars_5-10_seed1'
