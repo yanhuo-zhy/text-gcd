9425d6e2a4f26ff17721e6f0156c9626b7286d4f
yanhuo-zhy <1760331284@qq.com>
2024-01-27 17:02:05 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@
  # @Author: yanhuo 1760331284@qq.com
  # @Date: 2023-11-11 16:01:59
  # @LastEditors: yanhuo 1760331284@qq.com
- # @LastEditTime: 2024-01-27 17:00:44
+ # @LastEditTime: 2024-01-27 17:01:54
  # @FilePath: \text-gcd\scripts\train.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -14,7 +14,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_rebuttal_food_3-1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_rebuttal_food_1-3.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -27,10 +27,10 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --dataset_name='food' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=3 \
- --coteaching_epoch_i=1 \
+ --coteaching_epoch_t=1 \
+ --coteaching_epoch_i=3 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='rebuttal_food_3-1_seed1'
+ --experiment_name='rebuttal_food_1-3_seed1'
