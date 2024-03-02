0e3e696de2056a2c7c0b86a0fa4bd77161b06e30
yanhuo-zhy <1760331284@qq.com>
2024-03-02 16:11:56 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,13 +3,13 @@
  # @Author: yanhuo 1760331284@qq.com
  # @Date: 2023-11-11 16:01:59
  # @LastEditors: yanhuo 1760331284@qq.com
- # @LastEditTime: 2024-03-02 16:08:40
+ # @LastEditTime: 2024-03-02 16:11:47
  # @FilePath: \text-gcd\scripts\train.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
 #SBATCH --account cvl
 #SBATCH -p general
-#SBATCH --qos general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
