6d6fff2a9e8ea4812ffb86972ccc8e05488ce74b
yanhuo-zhy <1760331284@qq.com>
2024-03-02 15:47:46 +0800

    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@
  # @Author: yanhuo 1760331284@qq.com
  # @Date: 2023-11-11 16:01:59
  # @LastEditors: yanhuo 1760331284@qq.com
- # @LastEditTime: 2024-01-27 17:01:54
+ # @LastEditTime: 2024-03-02 15:47:24
  # @FilePath: \text-gcd\scripts\train.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -14,7 +14,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_rebuttal_food_1-3.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_eccv_scarstest.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -24,13 +24,13 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 # CUDA_VISIBLE_DEVICES=0 python get_tag_blip.py
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='food' \
+ --dataset_name='scars' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
- --coteaching_epoch_t=1 \
- --coteaching_epoch_i=3 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
  --seed_num=1 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='rebuttal_food_1-3_seed1'
+ --experiment_name='eccv_scarstest'
