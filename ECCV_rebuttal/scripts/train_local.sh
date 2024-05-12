9dddbf5bf0cd7174434571999520a6bceecc04f3
yanhuo-zhy <1760331284@qq.com>
2024-05-12 08:55:10 +0800

    rebuttal_trainnums&sh

---
+++ ECCV_rebuttal/scripts/train_local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0 python train_classnums.py \
+ --dataset_name='cub' \
+ --pseudo_ratio=0.6 \
+ --lambda_loss=0.2 \
+ --coteaching_epoch_t=10 \
+ --coteaching_epoch_i=15 \
+ --seed_num=1 \
+ --interrupted_path='' \
+ --batch_size=128 \
+ --prop_train_labels=0.5 \
+ --experiment_name='cub_test_baseparam'
