fb999fcec7015d5fc1020eaead9aac6c3d0d4e15
yanhuo-zhy <1760331284@qq.com>
2024-05-13 12:17:45 +0800

    sh_for_uon

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_test.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_scar_test.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -16,7 +16,7 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 # CUDA_VISIBLE_DEVICES=0 python train.py
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cub' \
+ --dataset_name='scars' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
@@ -25,4 +25,4 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='cub_test'
+ --experiment_name='scars_test'
