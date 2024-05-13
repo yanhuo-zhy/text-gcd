917c58f736f226748efdb8402bbb66501874e270
yanhuo-zhy <1760331284@qq.com>
2024-05-13 11:08:39 +0800

    sh_for_uon

--- ECCV_rebuttal/scripts/train_uon1.sh
+++ ECCV_rebuttal/scripts/train_uon1.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/rebuttal_cifar100_trainclassnums1.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/rebuttal_cifar100_trainclassnums2.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -20,9 +20,9 @@ CUDA_VISIBLE_DEVICES=0 python train_classnums.py \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
  --coteaching_epoch_i=15 \
- --seed_num=1 \
+ --seed_num=2 \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='cifar100_train_classnums_clip_seed1' \
+ --experiment_name='cifar100_train_classnums_clip_seed2' \
  --output_dir='./rebuttal/train_classnums' \
