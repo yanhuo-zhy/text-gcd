db1a22e97a87496e306e20c049777606083ab022
yanhuo-zhy <1760331284@qq.com>
2024-05-15 17:13:29 +0800

    testontest

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 #SBATCH --account cvl
-#SBATCH -p amp20
-#SBATCH --qos amp20
+#SBATCH -p general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cifar100_testontest.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_cub_testontest.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -16,7 +16,7 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 # CUDA_VISIBLE_DEVICES=0 python train.py
 CUDA_VISIBLE_DEVICES=0 python train.py \
- --dataset_name='cifar100' \
+ --dataset_name='cub' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
  --coteaching_epoch_t=10 \
@@ -25,4 +25,4 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
- --experiment_name='cifar100_testontest' \
+ --experiment_name='cub_testontest' \
