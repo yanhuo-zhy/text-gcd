f545516b36b5a910b51bcf97f0ee3e7c120938ab
yanhuo-zhy <1760331284@qq.com>
2024-05-15 16:44:56 +0800

    testontest

--- scripts/train_simgcd_uon_pszzz.sh
+++ scripts/train_simgcd_uon_pszzz.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_simgcd_cub_testontest_clip.txt
+#SBATCH -o /home/pszzz/hyzheng/text-gcd/temp/temp_simgcd_scars_testontest_clip.txt
 
 module load gcc/gcc-10.2.0
 # module load nvidia/cuda-10.0 nvidia/cudnn-v7.6.5.32-forcuda10.0
@@ -15,9 +15,9 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0 python SimGCD/train.py \
- --dataset_name='cub' \
+ --dataset_name='scars' \
  --seed_num=1 \
  --prop_train_labels=0.5 \
  --prop_knownclass=0.5 \
- --exp_name='SimGCD_cub_clip_testontest' \
+ --exp_name='SimGCD_scars_clip_testontest' \
  --print_freq=20
