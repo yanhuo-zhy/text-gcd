720b582e64287297f8fe5c8fe4bd15460a50b493
haiyang zheng <67444197+yanhuo-zhy@users.noreply.github.com>
2024-12-07 08:09:46 +0000

    iscap pets

--- scripts/iscap.sh
+++ scripts/iscap.sh
@@ -8,12 +8,12 @@
 #SBATCH --gres=gpu:1        # 4 gpus per node out of 4
 #SBATCH --mem=100000          # memory per node out of 494000MB (481GB)
 #SBATCH --job-name=pets_textgcd
-#SBATCH -o /leonardo_work/IscrC_Fed-GCD/hyzheng/text-gcd/pets_seed3.log
+#SBATCH -o /leonardo_work/IscrC_Fed-GCD/hyzheng/text-gcd/pets_seed0.log
 
 module load cuda/12.1
 source /leonardo/home/userexternal/hzheng00/miniconda3/bin/activate textgcd
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
+CUDA_VISIBLE_DEVICES=0 python train_knownclass.py \
  --dataset_name='pets' \
  --pseudo_ratio=0.6 \
  --lambda_loss=0.2 \
@@ -23,5 +23,6 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
  --interrupted_path='' \
  --batch_size=128 \
  --prop_train_labels=0.5 \
+ --prop_knownclass=0.5 \
  --experiment_name='pets_seed0' \
  --output_dir 'exp_iscap'
